d9a005c7c071897dea79b39a673f0e1c7a2b5868
scybulski <szymon9@windowslive.com>
2018-06-12 13:47:30 +0200

    Added ThrowsError constructor.

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,7 @@ import Numeric
 import Data.Ratio
 import Data.Complex
 import Data.Array
+import Control.Monad.Except
 
 data LispVal = Atom String
             | List [LispVal]
@@ -289,4 +290,27 @@ unpackNum _ = 0
 
 apply :: String -> [LispVal] -> LispVal
 apply func args = maybe (Bool False) ($ args) $ lookup func primitives  -- maybe returns false if lookup fails
+
+-- ERROR HANDLING
+data LispError = NumArgs Integer [LispVal]
+               | TypeMismatch String LispVal
+               | Parser ParseError
+               | BadSpecialForm String LispVal
+               | NotFunction String String
+               | UnboundVar String String
+               | Default String
+
+showError :: LispError -> String
+showError (UnboundVar message varname)  = message ++ ": " ++ varname
+showError (BadSpecialForm message form) = message ++ ": " ++ show form
+showError (NotFunction message func)    = message ++ ": " ++ show func
+showError (NumArgs expected found)      = "Expected " ++ show expected 
+                                       ++ " args; found values " ++ unwordsList found
+showError (TypeMismatch expected found) = "Invalid type: expected " ++ expected
+                                       ++ ", found " ++ show found
+showError (Parser parseErr)             = "Parse error at " ++ show parseErr
+
+instance Show LispError where show = showError
+
+type ThrowsError = Either LispError
 
