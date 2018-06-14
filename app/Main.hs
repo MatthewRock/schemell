ef42ee1197aa9c59459ded3bd2764110581ef133
MatthewRock <maliszmat@gmail.com>
2018-06-14 14:08:32 +0200

    Add if operator

--- app/Main.hs
+++ app/Main.hs
@@ -231,6 +231,18 @@ eval val@(String _) = return val  -- _ = don't care, match everything
 eval val@(Number _) = return val
 eval val@(Bool _) = return val
 eval (List [Atom "quote", val]) = return val
+eval (List [Atom "if", pred, conseq, alt]) =
+  do result <- eval pred
+     case result of
+       Bool False -> eval alt
+       otherwise -> eval conseq
+
+eval (List [Atom "if", pred, conseq]) =
+  do result <- eval pred
+     case result of
+       Bool False -> eval result
+       otherwise -> eval conseq
+
 eval (List (Atom func : args)) = mapM eval args >>= apply func
 eval badForm = throwError $ BadSpecialForm "Unrecognized special form" badForm
 
