aea9397def7c8edab3f32cf86de548297070c07c
MatthewRock <maliszmat@gmail.com>
2018-06-15 09:44:16 +0200

    Implemented REPL

--- app/Main.hs
+++ app/Main.hs
@@ -8,6 +8,7 @@ import Data.Ratio
 import Data.Complex
 import Data.Array
 import Control.Monad.Except
+import System.IO
 
 data LispVal = Atom String
             | List [LispVal]
@@ -22,11 +23,40 @@ data LispVal = Atom String
             | Vector (Array Int LispVal)
 
 
+-- REPL helpers
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+
+evalString :: String -> IO String
+evalString expr = return $ extractValue $ trapError (liftM show $ readExpr expr >>= eval)
+
+evalAndPrint :: String -> IO ()
+evalAndPrint expr = evalString expr >>= putStrLn
+
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ pred prompt action = do
+   result <- prompt
+   if pred result
+      then return ()
+      else action result >> until_ pred prompt action
+
+runRepl :: IO ()
+runRepl = until_ (== "quit") (readPrompt "Lisp>>> ") evalAndPrint
+
+
+-- Interpreter code
+
 main :: IO ()
 main = do
   args <- getArgs
-  evaled <- return $ liftM show $ readExpr (args !! 0) >>= eval
-  putStrLn $ extractValue $ trapError evaled
+  case length args of
+    0 -> runRepl
+    1 -> evalAndPrint $ args !! 0
+    otherwise -> putStrLn "Program takes only 0 or 1 arguments."
+
 
 symbol :: Parser Char
 symbol = oneOf "!$%&|*+-/:<=>?@^_~"
@@ -430,6 +460,7 @@ instance Show LispError where show = showError
 
 type ThrowsError = Either LispError
 
+trapError :: (Show a, MonadError a m) => m String -> m String
 trapError action = catchError action (return . show)
 
 extractValue :: ThrowsError a -> a
